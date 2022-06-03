8435e5dd7f55194795852fc3928dd2b5300e9063
yuchan1120 <nogginpanic@gmail.com>
2022-06-04 08:53:30 +0900

    CleaningWorkモデルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_03_231310) do
+ActiveRecord::Schema.define(version: 2022_06_03_235246) do
+
+  create_table "cleaning_works", force: :cascade do |t|
+    t.string "name"
+    t.integer "time_required"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "deliver_works", force: :cascade do |t|
     t.string "name"
