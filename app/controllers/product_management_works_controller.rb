12d3740cb8796f0ea28a9199260c094d3a14d29f
yuchan1120 <nogginpanic@gmail.com>
2022-06-23 23:51:28 +0900

    商品管理業務の作成、編集、削除機能を追加

--- app/controllers/product_management_works_controller.rb
+++ app/controllers/product_management_works_controller.rb
@@ -2,4 +2,43 @@ class ProductManagementWorksController < ApplicationController
   def index
     @product_management_works = ProductManagementWork.all
   end
+
+  def new
+    @product_management_work = ProductManagementWork.new
+  end
+
+  def create
+    @product_management_work = ProductManagementWork.new(product_management_work_params)
+    if @product_management_work.save
+      flash[:notice] = "商品管理業務を新規登録しました"
+      redirect_to :product_management_works
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @product_management_work = ProductManagementWork.find(params[:id])
+  end
+
+  def update
+    @product_management_work = ProductManagementWork.find(params[:id])
+    if @product_management_work.update(product_management_work_params)
+      flash[:notice] = "IDが「#{@product_management_work.id}」の商品管理業務を更新しました"
+      redirect_to :product_management_works
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @product_management_work = ProductManagementWork.find(params[:id])
+    @product_management_work.destroy
+    flash[:notice] = "商品管理業務を削除しました"
+    redirect_to :product_management_works
+  end
+
+  def product_management_work_params
+    params.require(:product_management_work).permit(:name, :time_required)
+  end
 end
