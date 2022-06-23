a683665dcd417023a7f79c223d0b1d728ecb8511
yuchan1120 <nogginpanic@gmail.com>
2022-06-23 21:27:36 +0900

    定時業務に関するフラッシュメッセージ表示機能を追加

--- app/controllers/regular_works_controller.rb
+++ app/controllers/regular_works_controller.rb
@@ -10,6 +10,7 @@ class RegularWorksController < ApplicationController
   def create
     @regular_work = RegularWork.new(params.require(:regular_work).permit(:name, :time_required))
     if @regular_work.save
+      flash[:notice] = "定時業務を新規登録しました"
       redirect_to :regular_works
     else
       render "new"
@@ -23,6 +24,7 @@ class RegularWorksController < ApplicationController
   def update
     @regular_work = RegularWork.find(params[:id])
     if @regular_work.update(params.require(:regular_work).permit(:name, :time_required))
+      flash[:notice] = "IDが「#{@regular_work.id}」の定時業務を更新しました"
       redirect_to :regular_works
     else
       render "edit"
@@ -32,6 +34,7 @@ class RegularWorksController < ApplicationController
   def destroy
     @regular_work = RegularWork.find(params[:id])
     @regular_work.destroy
+    flash[:notice] = "定時業務を削除しました"
     redirect_to :regular_works
   end
 end
