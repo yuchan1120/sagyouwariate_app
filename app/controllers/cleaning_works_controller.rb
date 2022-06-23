b457105849a60f2e03d8bede032491f46b42372d
yuchan1120 <nogginpanic@gmail.com>
2022-06-24 00:10:31 +0900

    清掃業務の作成、編集、削除機能を追加

--- app/controllers/cleaning_works_controller.rb
+++ app/controllers/cleaning_works_controller.rb
@@ -2,4 +2,42 @@ class CleaningWorksController < ApplicationController
   def index
     @cleaning_works = CleaningWork.all
   end
+  def new
+    @cleaning_work = CleaningWork.new
+  end
+
+  def create
+    @cleaning_work = CleaningWork.new(cleaning_work_params)
+    if @cleaning_work.save
+      flash[:notice] = "清掃業務を新規登録しました"
+      redirect_to :cleaning_works
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @cleaning_work = CleaningWork.find(params[:id])
+  end
+
+  def update
+    @cleaning_work = CleaningWork.find(params[:id])
+    if @cleaning_work.update(cleaning_work_params)
+      flash[:notice] = "IDが「#{@cleaning_work.id}」の清掃業務を更新しました"
+      redirect_to :cleaning_works
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @cleaning_work = CleaningWork.find(params[:id])
+    @cleaning_work.destroy
+    flash[:notice] = "清掃業務を削除しました"
+    redirect_to :cleaning_works
+  end
+
+  def cleaning_work_params
+    params.require(:cleaning_work).permit(:name, :time_required)
+  end
 end
