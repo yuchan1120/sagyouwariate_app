436144cda1683bf6551f04e666b36800a64fbfdf
yuchan1120 <nogginpanic@gmail.com>
2022-06-23 20:32:44 +0900

    定時業務の新規登録機能を追加

--- app/controllers/regular_works_controller.rb
+++ app/controllers/regular_works_controller.rb
@@ -2,4 +2,17 @@ class RegularWorksController < ApplicationController
   def index
     @regular_works = RegularWork.all
   end
+
+  def new
+    @regular_work = RegularWork.new
+  end
+
+  def create
+    @regular_work = RegularWork.new(params.require(:regular_work).permit(:name, :time_required))
+    if @regular_work.save
+      redirect_to :regular_works
+    else
+      render "new"
+    end
+  end
 end
