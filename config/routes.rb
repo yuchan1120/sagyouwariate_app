606c845d1e0524ab847207983a00aae3330146c1
yuchan1120 <nogginpanic@gmail.com>
2022-06-26 19:09:54 +0900

    アカウント作成時にデフォルトの業務が登録されるように修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+  }
   get 'cleaning_works/index'
   get 'product_management_works/index'
   root 'top#index'
